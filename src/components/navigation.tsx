b3bf0fc9c953b2c3f8aec1e0cfd6545c2e5e7dd7
Kanzoonrkz <kanzoonrkz@gmail.com>
2024-02-27 13:29:17 +0700

    refined: header and footer (desktop)

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -13,13 +13,13 @@ export default async function Navigation() {
 	const nav = await queryNavigation();
 
 	return (
-		<div className="sticky top-0 z-10 px-6 py-4 bg-black">
-			<nav className="relative flex items-center justify-center max-w-6xl mx-auto md:justify-between">
-				<Link className="text-4xl" href={"/"}>
+		<div className="sticky top-0 z-10 bg-light">
+			<nav className="relative flex items-center justify-center py-4 ccontainer md:justify-between">
+				<Link className="text-3xl font-marcellus" href={"/"}>
 					B100g
 				</Link>
 				<MobileNavToggle nav={nav} />
-				<ul className="items-center hidden gap-12 md:flex">
+				<ul className="items-center hidden gap-16 md:flex">
 					{nav.data.nav_group.map((link: any) => (
 						<li key={link.id}>
 							<PrismicNextLink className="p-2" field={link.nav_link}>
