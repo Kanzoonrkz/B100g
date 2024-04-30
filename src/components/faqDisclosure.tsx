c547e087cddf0cdf170e493c9f944f68acb3083d
Kanzoonrekza <kanzoonrkz@gmail.com>
2024-04-30 14:47:32 +0700

    fix faq disclosure spacing

--- src/components/faqDisclosure.tsx
+++ src/components/faqDisclosure.tsx
@@ -8,13 +8,13 @@ export default function FaqDisclosure(data: any) {
 	return (
 		<Disclosure>
 			{({ open }) => (
-				<div className="px-4">
-					<Disclosure.Button className="flex justify-between w-full gap-4 py-2 text-xl font-marcellus">
+				<div className="grid gap-2 px-4 py-4">
+					<Disclosure.Button className="flex justify-between w-full gap-4 text-xl font-marcellus">
 						<span>{question}</span>
 						<span className={open ? "rotate-90 transform" : ""}>V</span>
 					</Disclosure.Button>
-					<Disclosure.Panel className="px-3 text-gray-500">
-						<span className="prose prose-p:mb-1 prose-p:mt-2">
+					<Disclosure.Panel className="pr-5 text-gray-500">
+						<span className="prose prose-p:m-0">
 							<PrismicRichText field={answer} />
 						</span>
 					</Disclosure.Panel>
